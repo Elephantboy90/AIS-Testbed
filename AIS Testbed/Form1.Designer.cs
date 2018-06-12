9c8b4701dd99e339e64c92597ddb855013ad96de
rab <rab@PC-5362>
2018-06-12 09:56:17 +0100

    Add project files.

---
+++ AIS Testbed/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace AIS_Testbed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.tbIPAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDelay = new System.Windows.Forms.TextBox();
+            this.lblNumMessages = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btnStopCapture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // btnGO
+            // 
+            this.btnGO.Location = new System.Drawing.Point(12, 28);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(79, 31);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "Start Capture";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbIPAddress
+            // 
+            this.tbIPAddress.Location = new System.Drawing.Point(12, 104);
+            this.tbIPAddress.Name = "tbIPAddress";
+            this.tbIPAddress.Size = new System.Drawing.Size(100, 20);
+            this.tbIPAddress.TabIndex = 1;
+            this.tbIPAddress.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(131, 104);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 3;
+            this.tbPort.Text = "2233";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Delay (secs)";
+            // 
+            // tbDelay
+            // 
+            this.tbDelay.Location = new System.Drawing.Point(12, 165);
+            this.tbDelay.Name = "tbDelay";
+            this.tbDelay.Size = new System.Drawing.Size(100, 20);
+            this.tbDelay.TabIndex = 5;
+            this.tbDelay.Text = "0";
+            // 
+            // lblNumMessages
+            // 
+            this.lblNumMessages.AutoSize = true;
+            this.lblNumMessages.Location = new System.Drawing.Point(340, 28);
+            this.lblNumMessages.Name = "lblNumMessages";
+            this.lblNumMessages.Size = new System.Drawing.Size(106, 13);
+            this.lblNumMessages.TabIndex = 7;
+            this.lblNumMessages.Text = "Number of messages";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(12, 213);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbMessage.Size = new System.Drawing.Size(434, 150);
+            this.tbMessage.TabIndex = 8;
+            // 
+            // btnStopCapture
+            // 
+            this.btnStopCapture.Location = new System.Drawing.Point(117, 28);
+            this.btnStopCapture.Name = "btnStopCapture";
+            this.btnStopCapture.Size = new System.Drawing.Size(79, 31);
+            this.btnStopCapture.TabIndex = 9;
+            this.btnStopCapture.Text = "Stop Capture";
+            this.btnStopCapture.UseVisualStyleBackColor = true;
+            this.btnStopCapture.Click += new System.EventHandler(this.btnStopCapture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 375);
+            this.Controls.Add(this.btnStopCapture);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.lblNumMessages);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbDelay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbIPAddress);
+            this.Controls.Add(this.btnGO);
+            this.Name = "Form1";
+            this.Text = "AIS Port Capture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.TextBox tbIPAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDelay;
+        private System.Windows.Forms.Label lblNumMessages;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btnStopCapture;
+    }
+}
+
---
+++ AIS Testbed/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpAis;
+
+namespace AIS_Testbed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Boolean stop = false;
+        static private string getData(string Address, Int32 Port)
+        {
+            Application.DoEvents();
+            System.Net.IPAddress serverAddress = System.Net.IPAddress.Parse(Address);
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(serverAddress, Port);
+            NetworkStream ourStream = tcpClient.GetStream();
+            byte[] data = new byte[tcpClient.ReceiveBufferSize];         
+            // Read() is a blocking call
+            int bytesRead = ourStream.Read(data, 0, System.Convert.ToInt32(tcpClient.ReceiveBufferSize));
+           string strData = Encoding.ASCII.GetString(data, 0, bytesRead);
+            return strData;
+        }
+        private void Delay(long delay)
+        {
+            DateTime tthen = DateTime.Now;
+            do
+            {
+                Application.DoEvents();
+            } while (tthen.AddSeconds(delay) > DateTime.Now);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            Int32 port=2233;
+            string target="127.0.0.1";
+            Int64 numMessages = 0;
+
+            if (tbIPAddress.Text == "")
+            {
+                MessageBox.Show("Please enter a valid IP address");
+            }
+            else
+            {
+                target = tbIPAddress.Text;
+            }
+            if (tbPort.Text == "")
+            {
+                MessageBox.Show("Please enter a valid port number");
+            }
+            else
+            {
+                port = Convert.ToInt32(tbPort.Text);
+            }
+            while ((line = getData(target, port)) != null)
+            {
+                if (stop)
+                {
+                    stop = false;
+                    return;
+                }
+                Delay(Convert.ToInt64(tbDelay.Text));
+                var ht = new Hashtable();
+                var AisParser = new Parser();
+                ht = AisParser.Parse(line);
+                if (ht!=null)
+                { 
+                foreach (DictionaryEntry entry in ht)
+                {
+                        if (entry.Key.ToString()=="MMSI")
+                        { tbMessage.Text += Environment.NewLine;
+                          tbMessage.Text += Environment.NewLine;
+                          lblNumMessages.Text = Convert.ToString(numMessages++);
+                        }
+                        tbMessage.Text+= entry.Key + "  :  " + entry.Value.ToString() + Environment.NewLine;
+
+                    }
+                }
+                
+               
+            }
+        }
+
+        private void btnStopCapture_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+    
+}
